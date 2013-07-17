109de704f81da1f47264c435a680c8c9a86231ed
zeroDivisible <zerodivisible@gmail.com>
2013-07-17 22:31:51 +0100

    cleaned and fixed the code a little

--- numbers_to_words.py
+++ numbers_to_words.py
@@ -28,66 +28,69 @@ nums = { 0: "zero",
          80: "eighty",
          90: "ninety" }
 
-bignums = ["quintillion", "quadrillion", "trillion", "billion", "million", "thousand"]
+bignums = ["thousand", "million", "billion", "trillion", "quadrillion", "quintillion"]
 
 def to_words(number): 
-   stack = []
-   wordstack = []
-   word_num = ""
+   stack, wordstack, word_num = [], [], []
    while number:
       stack.append(number % 1000)
       number = number / 1000
    stack.reverse()
 
    while stack:
-      num = stack.pop()
-      wordstack.append(describe_triplet(num))
+      wordstack.append(describe_triplet(stack.pop()))
 
-   print(wordstack)
-   index = -1
    while wordstack:
-      word_num += wordstack.pop()
+      description = wordstack.pop()
+      if len(description) == 0:
+         continue
+
       if wordstack:
-         word_num += " " + bignums[index] + " "
-         index -= 1
-   return word_num
+         which_bignum = len(wordstack) - 1
+         word_num.append(description + " " + bignums[which_bignum])
+      else:
+         word_num.append(description)
+
+   solution = ""
+   while word_num:
+      solution += word_num.pop(0)
+      if len(word_num) == 1:
+         solution += " and "
+      else:
+         solution += " "
+
+   return solution
+   
 
 def describe_triplet(num):
-   # 123
-   # 120
-   h = num / 100
+   hundreds = num / 100
    num = num % 100
-   d = num / 10
-   r = num % 10
+   tens = num / 10
+   reminder = num % 10
 
-   print ("%s | %s | %s" % (h, d, r))
-   word_num = ""
+   word_num = []
 
-   if h:
-      word_num += "%s hundred" % (nums[h],)
+   if hundreds:
+      word_num.append("%s hundred" % (nums[hundreds],))
+   if num and hundreds:
+      word_num.append("and")
 
-   if h and (d or r):
-      word_num += " and "
-
-   if 10 < (d * 10 + r) < 20:
-      word_num += "%s" % (nums[d * 10 + r],)
+   if 10 < (num) < 20:
+      word_num.append("%s" % (nums[num],))
    else:
-      if d:
-         word_num += "%s" % (nums[d * 10],)
-      if (h or d):
-         word_num += " "
-      if r:
-         word_num += "%s" % (nums[r],)
-
-   return word_num
+      if tens:
+         word_num.append("%s" % (nums[tens * 10],))
+      if reminder:
+         word_num.append("%s" % (nums[reminder],))
+   return " ".join(word_num)
    
+
 def main():
    tests = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,
          30,32,40,43,50,54,60,65,70,76,80,87,90,98,101,100,199,1000,1001,1099,10000,10001,10099,
          100000,100001,1000000,1000001,1000099,1001001,1099001,1010001,101000001,999999999]
    for test in tests:
       print("%s=%s" % (test, to_words(test)))
-      print('\n')
 
 if __name__ == "__main__":
    main()
