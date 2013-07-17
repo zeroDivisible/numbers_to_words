432d082ca3ac23c62174211a5ab2db2ccb9188f3
zeroDivisible <zerodivisible@gmail.com>
2013-07-17 22:42:46 +0100

    added fancy ordering for numbers! :)

--- numbers_to_words.py
+++ numbers_to_words.py
@@ -1,32 +1,10 @@
 #!/usr/local/bin/python
-nums = { 0: "zero",
-         1: "one",
-         2: "two",
-         3: "three",
-         4: "four",
-         5: "five",
-         6: "six",
-         7: "seven",
-         8: "eight",
-         9: "nine",
-         10: "ten",
-         11: "eleven",
-         12: "twelve",
-         13: "thirteen",
-         14: "fourteen",
-         15: "fifteen",
-         16: "sixteen",
-         17: "seventeen",
-         18: "eighteen",
-         19: "nineteen",
-         20: "twenty",
-         30: "thirty",
-         40: "forty",
-         50: "fifty",
-         60: "sixty",
-         70: "seventy",
-         80: "eighty",
-         90: "ninety" }
+nums = { 0: "zero",     1: "one",      2: "two",        3: "three",     4: "four",
+         5: "five",     6: "six",      7: "seven",      8: "eight",     9: "nine",
+         10: "ten",     11: "eleven",  12: "twelve",    13: "thirteen", 14: "fourteen",
+         15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen",
+         20: "twenty",  30: "thirty",  40: "forty",     50: "fifty",    60: "sixty",
+         70: "seventy", 80: "eighty",  90: "ninety" }
 
 bignums = ["thousand", "million", "billion", "trillion", "quadrillion", "quintillion"]
 
@@ -44,7 +22,6 @@ def to_words(number):
       description = wordstack.pop()
       if len(description) == 0:
          continue
-
       if wordstack:
          which_bignum = len(wordstack) - 1
          word_num.append(description + " " + bignums[which_bignum])
